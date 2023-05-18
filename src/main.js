d3ca95643e80d1299736324de0fbf27d0fec1d0e
Planethac <info@planethac.de>
2023-05-18 09:03:59 +0200

    new file:   node_modules/.package-lock.json     new file:   node_modules/fs/README.md     new file:   node_modules/fs/package.json     new file:   node_modules/readline/.npmignore     new file:   node_modules/readline/README.md     new file:   node_modules/readline/package.json     new file:   node_modules/readline/readline.js     new file:   node_modules/readline/test/fixtures/afile.txt     new file:   node_modules/readline/test/fixtures/file-in-win1251.txt     new file:   node_modules/readline/test/fixtures/nmbr.txt     new file:   node_modules/readline/test/test_readline.js     modified:   package-lock.json     modified:   package.json     modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -4,3 +4,31 @@
 // TODO Start with The Project
 // ! Dont ask Other Peopel The help You, You not that bad at coding my friend!
 // as motivation take this: https://music.apple.com/library/playlist/p.oOzAaQPHE4J4qJA
+
+const fs = require('fs');
+
+let pckpth = ".";
+
+const readline = require('readline').createInterface ({
+    input: process.stdin,
+    output: process.stdout 
+});
+
+readline.question('Folder path: ', path => {
+    pckpth = `./${path}`;
+    readline.close();
+});
+
+fs.access(pckpth, (error) => {
+    if (error) {
+        fs.mkdir(pckpth , (error) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(`Succefuly created ${pckpth} !`);
+            }
+        });
+    } else {
+        console.log(`Directory ${pckpth} already exists !`)
+    }
+})
