34b03dfaa01b64ef351be49bc90112ba7d6ce2a0
Planethac <info@planethac.de>
2023-05-18 08:18:06 +0200

    modified:   src/main.js

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// App Goals:
+// Make a WORKING Pacakge controle System
+//
+// TODO Start with The Project
+// ! Dont ask Other Peopel The help You, You not that bad at coding my friend!
+// as motivation take this: https://music.apple.com/library/playlist/p.oOzAaQPHE4J4qJA
